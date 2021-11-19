b1a94e026fdb20420c135b23770d6af2b62d81a0
Vinicius Pietscher <vini.914@gmail.com>
2021-11-18 22:59:50 -0500

    Serve Static Assets

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get("/", function(req, res) {
+  // res.send("Hello Express")
+  // absolutePath = __dirname + "views/index.html"
+  res.sendFile(__dirname+"/views/index.html")
+});
+
+app.use("/public", express.static(__dirname + "/public"))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
